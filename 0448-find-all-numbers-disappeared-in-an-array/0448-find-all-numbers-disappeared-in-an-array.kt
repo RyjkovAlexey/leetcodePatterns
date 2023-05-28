60b2b6dfb8a0f93f8a3a97d4c5fb59e0264d68ee
Alexey Ryzhkov <alexey.ryjkov98@gmail.com>
2023-05-28 13:00:31 +0300

    Time: 2790 ms (7.48%), Space: 90.8 MB (23.81%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.kt
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.kt
@@ -1,17 +1,5 @@
 class Solution {
     fun findDisappearedNumbers(nums: IntArray): List<Int> {
-        val map = mutableMapOf<Int, Boolean>()
-
-        for (number in 1..nums.size) {
-            map[number] = false
-        }
-
-        nums.forEach {
-            if (map.containsKey(it)) {
-                map[it] = true
-            }
-        }
-
-        return map.filter { !it.value }.keys.toList()
+        return (1..nums.size).filter { !nums.contains(it) }
     }
 }
